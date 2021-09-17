a54f0b42074f6c2718990d5627fe2ab3b9ea5097
Jessica Choe <jessj.choe@gmail.com>
2021-09-17 23:28:56 +0900

    update readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const airtableBase = process.env.REACT_APP_AIRTABLE_BASE
+const airtableKey = process.env.REACT_APP_AIRTABLE_KEY
+const URL = `https://api.airtable.com/v0/${airtableBase}/Movies`
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${airtableKey}`
+  }
+}
 
 function App() {
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    const fetchMovies = async() => {
+      const res = await axios.get(URL, config)
+      console.log(res.data)
+    }
+    fetchMovies()
+  })
+  
+
   return (
     <div className="App">
-      HOME
+      <Route>
+        HOME
+      </Route>
     </div>
   );
 }
