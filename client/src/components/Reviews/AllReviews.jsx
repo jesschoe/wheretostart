68980259e90eea47cd1aa99dd5a6de173e62afd2
Jessica Choe <jessj.choe@gmail.com>
2021-09-25 10:18:15 +0900

    disabled delete for old reviews

--- client/src/components/Reviews/AllReviews.jsx
+++ client/src/components/Reviews/AllReviews.jsx
@@ -4,36 +4,71 @@ import './AllReviews.css'
 
 export default function AllReviews(props) {
     const [ reviews, setReviews ] = useState([])
-    
+
     useEffect(() => {
         if (props.reviewIds) {
             props.reviewIds.map(async (reviewId) => {
                 const data = await getReviews(reviewId)
+                
+                let time = data.createdTime.split('')
+                for (let i = 0; i < time.length; i++) {
+                    if (time[i] === 'T') {
+                        time[i] = ' '
+                    } else if (time[i] === '.') {
+                        time.splice([i], 5)
+                    }
+                }
+                console.log(time)
+                const compareTimes = time
+                time = time.join('')
+
+                for (let i = 0; i < compareTimes.length; i++) {
+                    if (isNaN(compareTimes[i]) || compareTimes[i] === ' ') {
+                        compareTimes.splice(i, 1)
+                    }
+                }
+                
                 const reviewObj = {
+                    title: data.fields.title,
                     username: data.fields.username,
                     review: data.fields.review,
-                    id: reviewId
+                    id: reviewId,
+                    time: time,
+                    compareTimes: compareTimes
                 }
                 setReviews((prevState) => [...prevState, reviewObj])
         })}
-        
+
     }, [props.reviewIds])
 
-    const handleDelete = async(id) => {  
+    const handleDelete = async(id) => {
         await deleteReview(id)
         window.location.reload();
     }
 
+    function compare( rev1, rev2 ) {
+        if ( rev1.compareTimes < rev2.compareTimes){
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    reviews.sort(compare)
+
     return (
         <div className='reviews-container'>
             <h3>REVIEWS</h3>
-            {reviews.map((review) => 
+            {reviews.map((review, i) =>
                 <div className='reviews' key={review.id}>
                     <p className='review-text'>{review.review}</p>
                     <p className='review-name'>-{review.username}</p>
-                    <button onClick={()=>handleDelete(review.id)}>delete</button>
-                </div>
-            ) 
+                    <p className='review-time'>{review.time}</p>
+                    {i===0 ? <button onClick={()=>handleDelete(review.id)}>delete</button> : 
+                        <p className='disabled-btn'>delete</p> }
+                </div>) 
+            
             }
         </div>
     )
