95723fdcab02859386f18f125971b6280b0ba50b
Jessica Choe <jessj.choe@gmail.com>
2021-09-26 05:27:41 +0900

    remove swipe

--- client/src/components/Carousel/Slider.js
+++ client/src/components/Carousel/Slider.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import Swipe from 'react-easy-swipe';
 import { fetchMovies } from '../../services'
 import './Slider.css'
 
@@ -44,55 +43,34 @@ export default function Slider() {
         }
     }
 
-    // swipe functions for touch screens
-    function onSwipeStart() {
-    }
-    
-    function onSwipeMove(position) {
-        setXPosition(position.x)
-    }
-    
-    function onSwipeEnd() {
-        if (xPosition > 150) {
-            prevSlide()
-        } else if (xPosition < -150) {
-            nextSlide()
-        }
-    }
-
     return (
         <div className='slider-container'>
             {movies.map((movie,i) => {return (
-                    <div 
+                <div 
+                    key={movie.id} 
+                    className={slideIndex === i + 1 ? 'slide active' : 'slide'}
+                >
+                    <Link 
+                        to={`/movies/${movie.id}/${i+1}`} 
                         key={movie.id} 
-                        className={slideIndex === i + 1 ? 'slide active' : 'slide'}
+                        style={{ textDecoration: 'none' }}
                     >
-                        <Link 
-                            to={`/movies/${movie.id}/${i+1}`} 
-                            key={movie.id} 
-                            style={{ textDecoration: 'none' }}
+                        <div 
+                            style={{
+                                backgroundImage: `url(${movie.fields?.poster})`, 
+                                backgroundRepeat: 'no-repeat', 
+                                backgroundSize:`contain`}} 
+                            className='movie-card'
                         >
-                            <Swipe
-                                onSwipeStart={onSwipeStart}
-                                onSwipeMove={onSwipeMove}
-                                onSwipeEnd={onSwipeEnd}
-                            >
-                                <div 
-                                    style={{backgroundImage: `url(${movie.fields?.poster})`, 
-                                        backgroundRepeat: 'no-repeat', 
-                                        backgroundSize:`contain`}} 
-                                    className='movie-card'
-                                >
-                                    <div className='rank'>
-                                        <h3>#{i+1}</h3>
-                                    </div>
-                                    <div className='details'>
-                                        <h3>{movie.fields?.title} \{movie.fields?.year}\</h3>
-                                    </div>
-                                </div>
-                            </Swipe>
-                        </Link>
-                    </div> 
+                            <div className='rank'>
+                                <h3>#{i+1}</h3>
+                            </div>
+                            <div className='details'>
+                                <h3>{movie.fields?.title} \{movie.fields?.year}\</h3>
+                            </div>
+                        </div>
+                    </Link>
+                </div> 
             )})}
             <i className="fas fa-chevron-right arrow next" onClick={nextSlide}></i>
             <i className="fas fa-chevron-left arrow prev" onClick={prevSlide}></i> 
